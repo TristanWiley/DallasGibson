055c26f2ad74da706b3848900c95fadc86319cae
Tristan Wiley <tristan@tristanwiley.com>
2017-11-15 02:08:04 -0500

    Meow

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+var currentHash = window.location.hash.substr(1);
+$(document).ready(function () {
+    handleHash(currentHash);
+
+    $('a').click(function () {
+        var value = this.hash.substr(1);
+        handleHash(value);
+    });
+});
+
+function handleHash(hash) {
+    switch (hash) {
+        case "":
+            $("#container").load("partials/home.html", function () {}).hide().fadeIn();
+            break;
+        case "about":
+            $("#container").load("partials/about.html", function () {}).hide().fadeIn();
+            break;
+        case "portfolio":
+            // $("#container").load("partials/portfolio.html", function () {}).hide().fadeIn();
+        case "contact":
+            // $("#container").load("partials/contact.html", function () {}).hide().fadeIn();
+    }
+}
