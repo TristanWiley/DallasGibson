66345d324503f9b16d002a679ef7f0ffb35106d1
Tristan Wiley <tristan@tristanwiley.com>
2019-04-07 21:28:19 -0400

    update website with new layouts

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
 var currentHash = window.location.hash.substr(1)
 $(document).ready(function () {
-    handleHash(currentHash)
+  handleHash(currentHash)
 
-    $('a').click(function () {
-        var value = this.hash.substr(1)
-        handleHash(value)
-    })
+  $('a').click(function () {
+    var value = this.hash.substr(1)
+    handleHash(value)
+  })
 })
 
 function handleHash(hash) {
-    switch (hash) {
-        case "":
-            $("#container").load("partials/home.html", function () {}).hide().fadeIn()
-            break
-        case "about":
-            $("#container").load("partials/about.html", function () {}).hide().fadeIn()
-            break
-        case "portfolio":
-            $("#container").load("partials/portfolio.html", function () {}).hide().fadeIn()
-            break
-        case "contact":
-            $("#container").load("partials/contact.html", function () {}).hide().fadeIn()
-            break
-    }
+  switch (hash) {
+    case "":
+      $("#container").load("partials/home.html", function () { }).hide().fadeIn()
+      break
+    case "about":
+      $("#container").load("partials/about.html", function () { }).hide().fadeIn()
+      break
+    case "portfolio":
+      $("#container").load("partials/portfolio.html", function () { }).hide().fadeIn()
+      break
+    case "contact":
+      $("#container").load("partials/contact.html", function () { }).hide().fadeIn()
+      break
+  }
 }
